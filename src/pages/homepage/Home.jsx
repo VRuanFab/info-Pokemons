d5a93345f1f49d38242295aa5548d502c888c757
VRuanFab <victor.ruanfab@gmail.com>
2024-06-10 11:35:13 -0300

    import  api

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,10 +1,28 @@
+import api from "../../api/api"
 
+const test = api.get('pokemon/squirtle')
+.then((res)  =>  {
+    console.log(res.data)
+    let pokemon_image = document.createElement('img')
+    pokemon_image.src = res.data.sprites.front_default
+    document.getElementById('poke-image').appendChild(pokemon_image)
+    let poke_shiny = document.createElement('img')
+    poke_shiny.src = res.data.sprites.other.showdown.front_default
+    document.getElementById('poke-image').appendChild(poke_shiny)
+})
+
+test
 export default function Homepage(){
     return (
+        <>
         <div className="w-screen h-screen grid justify-center">
                 <div className="text-blue-500">
                     this is my mainpage
                 </div>
+                <div id="poke-image">
+
+                </div>
         </div>
+        </>
     )
 }
