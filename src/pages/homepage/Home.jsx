2fc4202062b2fb7ffe76c6d25981ccb6acda5057
VRuanFab <victor.ruanfab@gmail.com>
2024-06-10 10:24:38 -0300

    adding things

---
+++ src/pages/homepage/Home.jsx
@@ -0,0 +1,10 @@
+
+export default function Homepage(){
+    return (
+        <div className="w-screen h-screen grid justify-center">
+                <div className="text-blue-500">
+                    this is my mainpage
+                </div>
+        </div>
+    )
+}
