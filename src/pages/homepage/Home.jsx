8d54dec74d80269b0f5e06ed58d9f4319f1df430
VRuanFab <victor.ruanfab@gmail.com>
2024-06-17 13:03:19 -0300

    avanço

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,18 +1,27 @@
+import { useState } from "react"
 import api from "../../api/api"
 import Info from "./Info_part"
 
-// const test = api.get('pokemon/charizard')
-// .then((res)  =>  {
-//     console.log(res.data)
-// })
+let pokemon = {}
+api.get('pokemon/charizard')
+.then((res)  =>  {
+    console.log(res.data)
+    pokemon.nome = res.data.name
+    pokemon.img = res.data.sprites.front_default
+})
 
 export default function Homepage(){
     return (
         <>
         <div className="w-screen h-screen grid justify-center content-center">
-            <div className="flex">
-                <div id="poke-image" className="w-[25rem] h-[30rem] bg-red-800 rounded-l-2xl rounded-r-md pl-[2 px] relative">
-                    <div className="absolute right-9 top-4 w-[80%] h-[90%] bg-red-400">
+            <div className="w-screen h-screen flex justify-center">
+                <div id="poke-image" className="mt-[3%] w-[90%] h-[90%] bg-gray-700 rounded-md relative flex justify-center">
+                    <div className="mt-6 w-[90%] h-[90%] bg-gray-200 rounded-md">
+
+                        <div className="grid text-center w-fit">
+                            <img src={pokemon.img} alt="pokeImage" id="pokemon_img"/>
+                            <label htmlFor="pokemon_img">{pokemon.nome}</label>
+                        </div>
 
                     </div>
                 </div>
