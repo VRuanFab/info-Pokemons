70c137e4230e5e0b0b4f8c98799418d5009e06af
ruan fab <victor.ruanfab@gmail.com>
2024-06-18 06:17:46 -0300

    adding image and name

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react"
 import api from "../../api/api"
-import Info from "./Info_part"
+// import Info from "./Info_part"
 
-// const test = api.get('pokemon/charizard')
-// .then((res)  =>  {
-//     console.log(res.data)
-// })
+const [pokemon, setPokemon] = useState([])
+
+
+api.get('pokemon/charizard')
+.then((res) => {
+    console.log(res.data)
+    const poke_class = {}
+    poke_class.nome = res.data.name;
+    poke_class.img = res.data.sprites.front_default;
+    setPokemon(poke_class)
+})
 
 export default function Homepage(){
     return (
         <>
         <div className="w-screen h-screen grid justify-center content-center">
-            <div className="flex">
-                <div id="poke-image" className="w-[25rem] h-[30rem] bg-red-800 rounded-l-2xl rounded-r-md pl-[2 px] relative">
-                    <div className="absolute right-9 top-4 w-[80%] h-[90%] bg-red-400">
+            <div className="flex w-screen justify-center content-center">
+                <div id="poke-image" className="w-[80%] h-[60rem] bg-gray-600 rounded-md relative flex justify-center">
+                    <div className="mt-6 w-[95%] h-[95%] bg-gray-300 rounded-sm">
 
+                        <div className="grid content-center w-fit border-2 text-center">
+                            <img id="poke" src={pokemon.img} alt="imagem"/>
+                            <label htmlFor="poke"> {pokemon.nome} </label>
+                        </div>
+                        
                     </div>
                 </div>
 
