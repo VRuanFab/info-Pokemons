156be2142bb388720d87e5daf98bc3e453773e24
Victor Ruan Fabricio Cavalcante <victor.cavalcante@matriz.fornecedoramaquinas.com.br>
2024-09-04 14:07:41 -0300

    implement images

--- src/models/loading.jsx
+++ src/models/loading.jsx
@@ -1,9 +1,11 @@
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import greatball from "../assets/pokeballs/greatball.png"
 
 export default function Loading(){
     return(
         <div className="w-full h-full grid content-center items-center absolute top-0 left-0 z-30 justify-center border-rounded-lg bg-[#b3b3b3]">
-                <AiOutlineLoading3Quarters className="w-[80%] h-fit"/>
+                {/* <AiOutlineLoading3Quarters className="w-[80%] h-fit"/> */}
+                <img src={greatball}/>
             <p>Loading...</p>
             {/* <img src={AiOutlineLoading3Quarters} alt="" /> */}
         </div>
